6d52aa1117cfdb76a5a6437fd841f74f2abc771e
aruna-pothur <aruna.pothur@gmail.com>
2024-02-05 17:14:46 +0530

    field changes

--- Item.cs
+++ Item.cs
@@ -5,15 +5,14 @@ namespace AzureWebAPIWSD
     public class Item
     {
 
-        [JsonProperty(PropertyName = "id")]
+       [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
         [JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
-        [JsonProperty(PropertyName = "description")]
-        public string Description { get; set; }
-        [JsonProperty(PropertyName = "isComplete")]
-        public bool Completed { get; set; }
-
+        [JsonProperty(PropertyName = "designation")]
+        public string designation { get; set; }
+        [JsonProperty(PropertyName = "email")]
+        public string email { get; set; }
         [JsonProperty(PropertyName = "partition")]
         public string Partition { get; set; }
 
