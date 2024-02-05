febeaa253b59d162719bea7020145d9c25498031
sunil pothur <aruna.pothur@gmail.com>
2024-02-05 10:40:07 +0530

    init commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+
+namespace AzureWebAPIWSD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private Container cosmosDbContainer;
+
+    private string databaseId = "data";
+    private string containerName = "container";
+
+    public UserController(IConfiguration configuration)
+    {
+        var connectionString = configuration["ConnectionString"];
+        var client = new CosmosClient(connectionString);
+        this.cosmosDbContainer = client.GetContainer(databaseId, containerName);
+    }
+
+        // POST api/values
+        [HttpPost]
+        public async void Post([FromBody] User user)
+        {
+            await this.cosmosDbContainer.CreateItemAsync<User>(user);
+        }
+
+
+    }
+}
---
+++ User.cs
@@ -0,0 +1,11 @@
+﻿namespace AzureWebAPIWSD
+{
+    public class User
+    {
+        public string sapid { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+
+        public string designation { get; set; }
+    }
+}
